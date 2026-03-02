54fcc038a749c3f6749206823eae977dcc82986a
nourabosaif <abosaif.nany@gmail.com>
2026-03-02 18:37:26 +0200

    Initialize frontend

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import { HeroUIProvider } from "@heroui/react";
+
+function App() {
+  return (
+    
+    <HeroUIProvider>
+    <></>
+    </HeroUIProvider>
+  
+  );
+}
+
+export default App;
