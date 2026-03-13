d99f1f6e60c9fba6cb68d3e1ab6e0d457985d23e
gamila06 <gamalohatem@gmail.com>
2026-03-13 15:02:41 +0200

    remove unused comments from DiscoverPageAuth

--- src/pages/feed/discover/DiscoverPageAuth.tsx
+++ src/pages/feed/discover/DiscoverPageAuth.tsx
@@ -1,20 +1,3 @@
-// import TrackCard from "@/components/UI/Card";
-// import { mockDiscoverTracks } from "@/mocks/discover";
-
-// const DiscoverPageAuth = () => {
-//   return (
-//     <div className="min-h-screen w-full" style={{ backgroundColor: "#121212" }}>
-//       <div className="flex gap-4 p-6">
-//         {mockDiscoverTracks.map((track) => (
-//           <TrackCard key={track.id} track={track} />
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default DiscoverPageAuth;
-
 import TrackCard from "@/components/UI/Card";
 import HorizontalCarousel from "@/components/discover/HorizontalCarousel";
 import { mockDiscoverTracks } from "@/mocks/discover";
