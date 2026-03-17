36b75b8fff7a4616f83362978655cf0128e6ec60
Shahd-Yehya <shahdyehya82@gmail.com>
2026-03-17 07:32:51 +0200

    added HeroUIProvider in main and prevented duplicate createRoot calls

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { HeroUIProvider } from "@heroui/react";
 import "./index.css";
 import App from "./App.tsx";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
 async function enableMocking() {
   if (import.meta.env.DEV) {
-    const { worker } = await import('./services/mocks/browser');
+    const { worker } = await import("./services/mocks/browser");
     return worker.start({
-      onUnhandledRequest: 'bypass',
+      onUnhandledRequest: "bypass",
     });
   }
 }
+
 enableMocking().then(() => {
-  createRoot(document.getElementById('root')!).render(
+  createRoot(document.getElementById("root")!).render(
     <StrictMode>
-      <App />
+      <HeroUIProvider>
+        <App />
+      </HeroUIProvider>
     </StrictMode>,
   );
 });
