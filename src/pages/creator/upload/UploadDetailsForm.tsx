f9d3e96c73b3aee8f5a1535b159bf12533989d84
Mariam Ramy <mariam.said06@eng-st.cu.edu.eg>
2026-03-18 13:22:57 +0200

    added genre dropdown, description and privacy

--- src/pages/creator/upload/UploadDetailsForm.tsx
+++ src/pages/creator/upload/UploadDetailsForm.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { useAuthStore } from "@/stores/auth.store";
 import HelpIcon from "./HelpIcon";
 import UploadCoverImage from "./UploadCoverImage";
+import Dropdown from "@/components/UI/Dropdown";
+import GenreDropdown from "./GenreDropdown";
 
 interface Props {
   audioData: File | Blob | null;
@@ -16,6 +18,7 @@ const UploadDetailsForm = ({ audioData, onCancel }: Props) => {
 
   const { user } = useAuthStore();
   const username = user?.username || "username";
+  const [privacy, setPrivacy] = useState("public");
 
   return (
     <div className="mt-10 animate-in fade-in slide-in-from-bottom-4 duration-500">
@@ -36,7 +39,7 @@ const UploadDetailsForm = ({ audioData, onCancel }: Props) => {
           </div>
 
           <div>
-            <label className="block text-xs font-bold mb-1 tracking-wide uppercase">
+            <label className="block text-xs font-bold mb-1 tracking-wide ">
               Track link
             </label>
             <div className="flex items-center text-sm text-text-upload border-b border-border py-2">
@@ -45,7 +48,6 @@ const UploadDetailsForm = ({ audioData, onCancel }: Props) => {
               </span>
               <input
                 type="text"
-                readOnly
                 defaultValue={initialTitle.toLowerCase().replace(/\s+/g, "-")}
                 className="flex-1 bg-transparent text-text-upload outline-none px-1"
               />
@@ -66,6 +68,9 @@ const UploadDetailsForm = ({ audioData, onCancel }: Props) => {
             </p>
           </div>
           <div>
+            <GenreDropdown />
+          </div>
+          <div>
             <label className="flex items-center gap-1 text-xs font-bold mb-1 tracking-wide ">
               Tags <HelpIcon />
             </label>
@@ -74,6 +79,66 @@ const UploadDetailsForm = ({ audioData, onCancel }: Props) => {
               placeholder="Add styles, moods, tempo."
               className="w-full bg-transparent text-sm border-b border-border py-2 outline-none focus:border-white placeholder:text-text-upload/40"
             />
+          </div>
+          <div>
+            <label className="flex items-center gap-1 text-xs font-bold mb-1 tracking-wide ">
+              Description
+            </label>
+            <input
+              type="text"
+              placeholder="Tracks with description tend to get more plays and engagements."
+              className="w-full bg-transparent text-sm border-b border-border py-2 outline-none focus:border-white placeholder:text-text-upload/40"
+            />
+          </div>
+          <div>
+            <label className="flex items-center gap-1 text-xs font-bold mb-1 tracking-wide ">
+              Track Privacy
+            </label>
+            <div className="flex gap-10 text-sm py-2">
+              <label className="flex items-center gap-3 cursor-pointer group">
+                <input
+                  type="radio"
+                  name="privacy"
+                  className="hidden"
+                  checked={privacy === "public"}
+                  onChange={() => setPrivacy("public")}
+                />
+                <div
+                  className={`w-5 h-5 rounded-full border-2 flex items-center justify-center transition-all ${privacy === "public" ? "border-white" : "border-[#666] group-hover:border-white"}`}
+                >
+                  {privacy === "public" && (
+                    <div className="w-2.5 h-2.5 bg-bg-inverted rounded-full" />
+                  )}
+                </div>
+                <span
+                  className={`text-sm ${privacy === "public" ? "text-upload" : "text-[#999]"}`}
+                >
+                  Public
+                </span>
+              </label>
+
+              <label className="flex items-center gap-3 cursor-pointer group">
+                <input
+                  type="radio"
+                  name="privacy"
+                  className="hidden"
+                  checked={privacy === "private"}
+                  onChange={() => setPrivacy("private")}
+                />
+                <div
+                  className={`w-5 h-5 rounded-full border-2 flex items-center justify-center transition-all ${privacy === "private" ? "border-white" : "border-[#666] group-hover:border-white"}`}
+                >
+                  {privacy === "private" && (
+                    <div className="w-2.5 h-2.5 bg-bg-inverted rounded-full" />
+                  )}
+                </div>
+                <span
+                  className={`text-sm ${privacy === "private" ? "text-upload" : "text-[#999]"}`}
+                >
+                  Private
+                </span>
+              </label>
+            </div>
           </div>
         </div>
       </div>
