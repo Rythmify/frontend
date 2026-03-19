ccacc81b414f12e66980f371830682806e4c4b81
Shahd-Yehya <shahdyehya82@gmail.com>
2026-03-19 09:41:39 +0200

    handling MSW

--- src/services/mocks/handlers/index.ts
+++ src/services/mocks/handlers/index.ts
@@ -1,7 +1,9 @@
 import { messageHandlers } from './messageHandlers';
 import { authHandlers } from './authHandlers';
+import {trackHandlers} from './trackHandler';
 
 export const handlers = [
   ...authHandlers,
   ...messageHandlers,
+  ...trackHandlers
 ];
