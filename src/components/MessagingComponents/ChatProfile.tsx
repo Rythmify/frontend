22312806e4aea73376bbe1c8d8a3caf60e377a4d
farahsharshar <Farah.hassan05@eng-st.cu.edu.eg>
2026-04-11 14:05:49 +0200

    *added default profile picture if the user doesn't have one

--- src/components/MessagingComponents/ChatProfile.tsx
+++ src/components/MessagingComponents/ChatProfile.tsx
@@ -33,13 +33,15 @@ export function ChatProfile({ conversation, isActive = false, onClick }: ChatPro
     <span className="w-2.5 h-2.5 rounded-full bg-[#f50] block" />
   )}
 </div>
-        <img
-          src={participant.profile_picture}
-          alt={participant.display_name}
-          className="object-cover rounded-full w-11 h-11 "
-        />
-    
-       
+       {participant.profile_picture ? (
+  <img
+    src={participant.profile_picture}
+    alt={participant.display_name}
+    className="object-cover rounded-full w-11 h-11"
+  />
+) : (
+  <div className="w-11 h-11 rounded-full bg-gradient-to-br from-[#b08a8a] to-[#6b5b6b] flex-shrink-0" />
+)}
       </div>
 
       <div className="flex-1 min-w-0">
