50272961086e1406b275708b037a356e2c485d75
farahsharshar <Farah.hassan05@eng-st.cu.edu.eg>
2026-04-26 10:38:15 +0300

    *added the search sidebar to the searchpage

--- src/pages/search/SearchPage.tsx
+++ src/pages/search/SearchPage.tsx
@@ -1,13 +1,16 @@
 import { Outlet } from "react-router-dom";
-
+import SearchSidebar from "@/components/SearchComponents/Searchsidebar";
 /**
  * SearchPage – wrapper layout for all /search/* child routes.
  * Renders the active child route via <Outlet />.
  */
 export default function SearchPage() {
   return (
-    <div data-test="search-page" style={{ padding: "20px 25px" }}>
-      <Outlet />
+    <div data-test="search-page" style={{ padding: "20px 25px", flexDirection: "row", display: "flex" }}>
+    <SearchSidebar query={new URLSearchParams(window.location.search).get("q") ?? ""} />
+        <div className="flex-1">
+          tracks albums playlists users display part
+          </div>
     </div>
   );
 }
