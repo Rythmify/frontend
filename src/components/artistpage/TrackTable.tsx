9aba40e70fc8ed79effe842a6ba38a20fdf680d7
nourabosaif <abosaif.nany@gmail.com>
2026-04-30 19:23:13 +0300

    eslint error

--- src/components/artistpage/TrackTable.tsx
+++ src/components/artistpage/TrackTable.tsx
@@ -50,7 +50,7 @@ export function TrackTable({ tracks, filter, onDeleteTrack }: Props) {
   function toggleOne(id: string) {
     setSelected((prev) => {
       const next = new Set(prev);
-      next.has(id) ? next.delete(id) : next.add(id);
+      if (next.has(id)) { next.delete(id); } else { next.add(id); }
       return next;
     });
   }
