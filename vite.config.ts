28a09db7fc22277376c9514439de82a889508bcf
nourabosaif <abosaif.nany@gmail.com>
2026-05-02 01:26:45 +0300

    url error

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  cacheDir: 'C:/vite-cache/rythmify',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
